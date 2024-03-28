e7ab476c47da1f4a5d15bbe1db23f1e101603593
Andrea Ferrario <andre.ferrario@icloud.com>
2024-03-28 21:22:03 +0100

    Update open-the-gate.ps1
    added comments and corrected english typo

--- open-the-gate.ps1
+++ open-the-gate.ps1
@@ -1,16 +1,20 @@
+# Init Variables
 $wsl_ip = (wsl hostname -I).trim()
 $fwRuleName="Allow WSL Ports"
 $portArray = 1234, 8000, 8080, 9000, 9001, 9002
 Write-Host "WSL Machine IP: $wsl_ip"
 
+# Applying forwarding rules
 foreach($port in $portArray){
     Write-Host "Forwarding Port: $port to WSL"
     netsh interface portproxy add v4tov4 listenport=$port connectport=$port connectaddress=$wsl_ip
 }
+
+# Applying FW rules
 if (Get-NetFirewallRule -DisplayName $fwRuleName) {
-    Write-Host "Firewall rule already exist, reappling the rule to keep it updated"
+    Write-Host "Firewall rule already exist, re-applying the rule to keep it updated"
     Remove-NetFirewallRule -DisplayName  $fwRuleName
 } else {
-    Write-Host "Firewall rule not applied yet"
+    Write-Host "Firewall rule not applyed yet"
 }
 New-NetFirewallRule -DisplayName $fwRuleName -Direction Inbound -Action Allow -LocalPort $portArray -Protocol TCP
